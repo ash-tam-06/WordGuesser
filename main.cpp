ff0b529bb3afb8c4fd87f35ff34cb23541e4bf12
Ash Tamang <asmitatam2006@gmail.com>
2025-02-15 19:14:53 -0800

    First Commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -9,69 +10,53 @@ using namespace std;
 string listOfWords[] = {"place", "party", "cloud", "pinky", "stair", "heart"};
 string wordGuessed;
 int guessNum;
-string mysteryWord[5], wordGuessedArray[5], lettersInCorrectPlace[5], lettersInWrongPlace[5];
+string correctLetters, wrongPlaceLetters, guess, mystery;
 
-void makeGuessArray(string word) {
-    for (int i = 0; i < 5; i++) {
-        wordGuessedArray[i] = word.substr(i, 1);
-    }
-}
+
 
 void pickWord() {
     long elapsedSeconds = time(nullptr);
     srand(elapsedSeconds);
     int randNum = rand()%size(listOfWords);
-    string randomWord = listOfWords[randNum];
-    for (int i = 0; i < 5; i++) {
-        mysteryWord[i] = randomWord.substr(i, 1);
+    mystery = listOfWords[randNum];
+}
+
+
+void checkGuess(){
+    while(guessNum < 5){
+        for(int i = 0; i < 5; i++) {
+            if(guess.substr(i, 1) == mystery.substr(i, 1)) {
+                correctLetters +=  guess.substr(i,1) + " + ";
+
+            } else if (mystery.find(guess.substr(i, 1)) != string::npos) {
+                wrongPlaceLetters += guess.substr(i,1) + " + ";
+            }
+        }
+
+        if(correctLetters.length() == 10){
+            correctLetters = "";
+        }
+
+        cout << "These are the letters you got correct: " << correctLetters <<  endl;
+        cout << "These are the letters you got correct, but are in the wrong place "<< wrongPlaceLetters << endl;
+
+        guessNum++;
+        correctLetters = "";
+        wrongPlaceLetters = "";
     }
 }
 
 int main() {
 
-
     pickWord();
-    for (int i = 0; i<5; i++)
-        cout << mysteryWord[i] << endl;
 
-    while (guessNum < 1) {
-
-        cout<< "Please write your 5 letter word guess: " << endl;
-        cin>>wordGuessed;
-        makeGuessArray(wordGuessed);
-
-        for (int i = 0; i< size(wordGuessedArray); i++) {
-            if (mysteryWord[i] == wordGuessedArray[i]) {
-                lettersInCorrectPlace[i] = wordGuessedArray[i];
-            } else {
-                for (int j = i +1; j < size(wordGuessedArray); j++) {
-                    if (mysteryWord[i] == wordGuessedArray[j]) {
-                        lettersInWrongPlace[i] = wordGuessedArray[j];
-                        //TODO: get out of just this for loop
-                    }
-                }
-            }
-        }
-
-        cout << "These are the letters you got correct"<< endl;
-        for (int i = 0; i <5; i++) {
-            cout <<lettersInCorrectPlace[i] << "_";
-        }
-
-        cout << "These are the letters you got correct, but are in the wrong place"<< endl;
-        for (int i = 0; i <5; i++) {
-            cout <<lettersInWrongPlace[i] << "_";
-        }
-
-    guessNum++;
-    for (int i = 0; i< 5; i++) {
-        lettersInWrongPlace[i] = "_";
-        lettersInCorrectPlace[i] = "_";
-    }
-    }
+    cout << mystery << endl;
 
 
+    cout<< "Please write your 5 letter word guess: " << endl;
+    cin>>guess;
+
+    checkGuess();
 
     return 0;
 }
-
